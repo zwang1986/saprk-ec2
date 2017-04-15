1d16d1d3e9abba973b1782d0cda6509fb70d6eba
RobX <zwang1986@gmail.com>
2017-04-15 16:46:28 +0800

    add webUI prot parameter

--- spark_ec2.py
+++ spark_ec2.py
@@ -104,8 +104,8 @@ DEFAULT_SPARK_VERSION = SPARK_EC2_VERSION
 DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
-DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/amplab/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "branch-1.6"
+DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/zwang1986/spark-ec2"
+DEFAULT_SPARK_EC2_BRANCH = "master"
 
 
 def setup_external_libs(libs):
