a20bd104d44fbc781b5c95a37593b41e96ba2b31
RobX <zwang1986@gmail.com>
2017-07-21 23:36:58 +0800

    hadoop-2.6.0

--- spark_ec2.py
+++ spark_ec2.py
@@ -817,7 +817,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, opts, deploy_ssh_key):
             ssh_write(slave_address, opts, ['tar', 'x'], dot_ssh_tar)
 
     modules = ['spark', 'ephemeral-hdfs', 'persistent-hdfs',
-               'mapreduce', 'spark-standalone', 'tachyon', 'rstudio']
+               'mapreduce']
 
     if opts.hadoop_major_version == "1":
         modules = list(filter(lambda x: x != "mapreduce", modules))
